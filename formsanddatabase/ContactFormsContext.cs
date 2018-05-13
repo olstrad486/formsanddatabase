a9f2e90da89c79aea0ebc06378a16a570fb3e6c1
Oleksander Stradetskyi <ol.strad@outlook.com>
2018-05-14 01:42:16 +0300

    Initial commit forms and database

---
+++ formsanddatabase/ContactFormsContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace formsanddatabase
+{
+    public class ContactFormsContext : DbContext
+    {
+        public DbSet<ContactForm> ContactForms { get; set; }
+
+        public ContactFormsContext(DbContextOptions<ContactFormsContext> options)
+            : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=contactforms.db");
+        }
+    }
+    [Table("contactform")]
+    public class ContactForm
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ formsanddatabase/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace formsanddatabase.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ContactFormsContext _context;
+
+        public HomeController(ContactFormsContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ContactForm contact)
+        {
+            
+            _context.ContactForms.Add(contact);
+            _context.SaveChanges();
+            
+            return View();
+        }
+    }
+
+}
